0472f707c98dd16f0d68bea194ed5c57fe02d7b2
Maria-Backend <maria.ladesigner@gmail.com>
2026-02-21 15:29:04 +0000

    Refactor CSV reading logic

--- csv_to_json.py
+++ csv_to_json.py
@@ -3,11 +3,9 @@ import json
 import sys
 
 def csv_to_json(csv_file_path, json_file_path):
-    data = []
-    with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
+    with open(csv_file_path, encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
-        for row in reader:
-            data.append(row)
+        data = list(reader)
 
     with open(json_file_path, 'w', encoding='utf-8') as jsonfile:
         json.dump(data, jsonfile, indent=4)
